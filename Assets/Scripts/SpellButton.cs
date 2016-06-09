7976e1074c4128b975b8a51947fa63c64b859c94
Henon W. Chesser <henonchesser@yahoo.com>
2016-06-09 16:39:53 -0400

    initial commit

---
+++ Assets/Scripts/SpellButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellButton : MonoBehaviour {
+
+    [SerializeField]
+    private bool _isRune;
+    [SerializeField]
+    private Sprite _inactiveSprite;
+    [SerializeField]
+    private Sprite _activeSprite;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
